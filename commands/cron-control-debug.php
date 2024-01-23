ef3ae93fa63d7a4e42820bf60a506ac667af4218
Kailey Lampert <trepmal@gmail.com>
2024-01-22 17:15:44 -0800

    add cron-control debug

---
+++ commands/cron-control-debug.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Cron_Control_Debug
+ *
+ */
+
+class Cron_Control_Debug_CLI extends WP_CLI_Command {
+
+	/**
+	 * Check events in cron-control for serialization issues
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--per-page=<per-page>]
+	 * : Number of options from database to list. 'notoptions' are not counted
+	 * and are always displayed. Default: 1000
+	 *
+	 * [--page=<page>]
+	 * : Page of results
+	 *
+	 * [--format=<format>]
+	 * : Format to use for the output. One of table, csv or json.
+	 * ---
+	 * default: table
+	 * options:
+	 *   - table
+	 *   - json
+	 *   - csv
+	 *   - yaml
+	 *   - count
+	 * ---
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     $ wp cron-control debug
+	 *     # example output truncated
+	 *     +-----+--------+----------------------------------+----------------------------+-------------------------+----------------------------------------+
+	 *     | ID  | action | instance                         | args                       | args_ok                 | match                                  |
+	 *     +-----+--------+----------------------------------+----------------------------+-------------------------+----------------------------------------+
+	 *     | 123 | action | 49a3696adf0fbfacc12383a2d7400d51 | a:1:{s:3:"foo";s:3:"baz";} | ✅ args can unserialize | ❌ WARNING: args do not match instance |
+	 *     +-----+--------+----------------------------------+----------------------------+-------------------------+----------------------------------------+
+	 *
+	 */
+	function __invoke( $args, $assoc_args ) {
+
+		$limit = absint( WP_CLI\Utils\get_flag_value( $assoc_args, 'per-page', 1000 ) );
+		$page  = absint( WP_CLI\Utils\get_flag_value( $assoc_args, 'page', 1 ) );
+		$page_for_math = $page - 1;
+		$offset = $limit * $page_for_math;
+
+		global $wpdb;
+
+		$jobs_list = $wpdb->get_results(
+			$wpdb->prepare(
+				"SELECT SQL_CALC_FOUND_ROWS ID,action,action_hashed,instance,args from {$wpdb->prefix}a8c_cron_control_jobs where status = 'pending' ORDER BY timestamp DESC LIMIT %d OFFSET %d",
+				$limit,
+				$offset
+			)
+		);
+
+		$total_rows = $wpdb->get_var('SELECT FOUND_ROWS()');
+
+		$output = [];
+		$failures = 0;
+
+		foreach ( $jobs_list as $job ) {
+
+			$args_match   = ( $job->instance === md5( $job->args ) );
+			$args_ok      = unserialize( $job->args ) !== false;
+			$action_match = md5( $job->action ) === $job->action_hashed;
+
+			if ( ! $args_match || ! $args_ok || ! $action_match ) {
+				$failures++;
+				$output[ $job->ID ] = [
+					'ID'       => $job->ID,
+					'action'   => $job->action,
+					'instance' => $job->instance,
+					'args'     => $job->args,
+					'match'    => ( $args_match ? '✅ md5( args ) match instance' : "❌ WARNING: args do not match instance" ),
+					'args_ok'  => ( $args_ok ? '✅ args can unserialize' : "❌ WARNING: cannot unserialize" ),
+					'action_match'  => ( $action_match ? '✅ md5( action ) match action_hash' : "❌ WARNING: action does not match action_hashed" ),
+					
+				];
+			}
+
+		}
+
+		$format = $assoc_args['format'];
+		$formatter = new \WP_CLI\Formatter( $assoc_args, array( 'ID', 'action', 'instance', 'args', 'match', 'args_ok', 'action_match' ), 'cron-control-debug' );
+		$formatter->display_items( $output );
+
+		WP_CLI::line( 'Only displaying problematic events' );
+
+		if (
+			// don't show footer for strict formats (csv, json...)
+			// $format == 'table' && // $format not set until we wrap this in a proper command
+			$total_rows > ( $offset + $limit )
+		) {
+			WP_CLI::line( sprintf(
+				'Page %d/%d database options shown. Use `--per-page=%d --page=%d` for next set.',
+				$page,
+				ceil( $total_rows / $limit ),
+				$limit,
+				$page + 1
+			) );
+		}
+
+		$queue_size = defined( 'CRON_CONTROL_JOB_QUEUE_SIZE' ) && CRON_CONTROL_JOB_QUEUE_SIZE ? CRON_CONTROL_JOB_QUEUE_SIZE : 10;
+
+		if ( $queue_size <= $failures ) {
+			WP_CLI::warning( 'There are enough failures to clog the job queue. (queue size: '. $queue_size.')' );
+		}
+
+	}
+
+}
--- misc.php
+++ misc.php
@@ -1,10 +1,14 @@
 <?php
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
+
 	require 'commands/network-list.php';
 	WP_CLI::add_command( 'network list', 'Network_List_CLI' );
 
 	require 'commands/site-update.php';
 	WP_CLI::add_command( 'site update', 'Site_Update_CLI' );
+
+	require 'commands/cron-control-debug.php';
+	WP_CLI::add_command( 'cron-control debug', 'Cron_Control_Debug_CLI' );
 
 }
